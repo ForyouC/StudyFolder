96966143883e06a5888b8e4c9900fbdea4b639ea
Foryou <hjw7548@gmail.com>
2020-01-19 17:38:45 +0900

    2020/01/19/17:38 //javascript_practice

--- not_use_bootstrap.php
+++ not_use_bootstrap.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
   <title>Document</title>
   <link rel="stylesheet" href="not_use.css">
 </head>
+
 <body>
   <div class="my">
     <div class="container">
@@ -18,6 +20,16 @@
     <div class="container3">
       h1
     </div>
+    <script type="text/javascript">
+      function hello() {
+        var heloo = va;
+      }
+      <?php
+        phpinfo();
+      ?>
+
+    </script>
   </div>
 </body>
+
 </html>
